aec5d1762e09f6da9e3243816a37e32005a1dc11
lk <2725239384@qq.com>
2025-12-23 10:57:32 +0800

    1

--- Warrior/Assets/Script/SceneManager/LoadStartSceneScript.cs
+++ Warrior/Assets/Script/SceneManager/LoadStartSceneScript.cs
@@ -40,7 +40,7 @@ public class LoadStartSceneScript : MonoBehaviour
         startBtn.onClick.AddListener(() =>
         { 
 #if UNITY_EDITOR
-            LoginPram loginPram = new LoginPram { nickname = "g759993302207", adult_level = "4", timestamp = "1700000000" };
+            LoginPram loginPram = new LoginPram { nickname = "n76534474", adult_level = "2", timestamp = "1700000000" };
             SdkScript.nickname = loginPram.nickname;
             SdkScript.adult_level = Convert.ToInt32(loginPram.adult_level);
             ID(loginPram);
@@ -119,7 +119,8 @@ public class LoadStartSceneScript : MonoBehaviour
     private void ID(LoginPram data)
     {
         PlayerPrefs.SetInt(SdkScript.nickname + "CurrentPlayerAge", Convert.ToInt32(data.adult_level));
-        string str = "y75999322,4;f75999326,4;m75999331,4;g75999347,4;r75999384,4;k75999398,4;j75999307,3;s75999313,3;u75999318,3;d75999342,3;e75999379,3;a75999393,3;";
+        //3表示中级账号，4表示高级账号
+        string str = "y75999322,4;f75999326,4;m75999331,4;g75999347,4;r75999384,4;k75999398,4;z76534483,4;q76534487,4;j75999307,3;s75999313,3;u75999318,3;d75999342,3;e75999379,3;a75999393,3;s76534469,3;n76534474,3;";
         string[] list = str.Split(';');
         for (int i = 0; i < list.Length; i++)
         {
